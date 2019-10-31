4ff777bfe45ede8ed0a96fc9cd7d9093b8f897e2
CharlesS117 <57164802+CharlesS117@users.noreply.github.com>
2019-10-30 19:07:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(function() {
 
 	var app_id = '2667318313330971';
-	var scopes = 'email, user_friends, user_online_presence';
+	var scopes = 'email, user_friends';
 
 	var btn_login = '<a href="#" id="login" class="btn btn-primary">Iniciar sesión</a>';
 
